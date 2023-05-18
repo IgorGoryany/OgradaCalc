d967c414c681eb448b5576ce3fefe4cc3c9b193c
IgorGoryany <128911252+IgorGoryany@users.noreply.github.com>
2023-05-18 18:43:59 +0300

    Update choosingImage.js

--- js/support/choosingImage.js
+++ js/support/choosingImage.js
@@ -1,16 +1,27 @@
 import { sideXInput, sideYInput } from '../variables/variables';
 
+import img2000X3000 from '../../img/2000X3000.svg';
+import img2000X4000 from '../../img/2000X4000.svg';
+import img2500X2000 from '../../img/2500X2000.svg';
+import img3000X3000 from '../../img/3000X3000.svg';
+import img3300X2000 from '../../img/3300X2000.svg';
+import img3300X3000 from '../../img/3300X3000.svg';
+import img3300X4000 from '../../img/3300X4000.svg';
+import img4000X2000 from '../../img/4000X2000.svg';
+import img4000X3000 from '../../img/4000X3000.svg';
+import img4000X4000 from '../../img/4000X4000.svg';
+
 export function choosingImage() {
 	if (+sideXInput.value >= 3400 && +sideYInput.value > 3000) {
-		return '4000X4000';
+		return img4000X4000;
 	}
 
 	if (+sideXInput.value === 3000 && +sideYInput.value === 3000) {
-		return '3000X3000';
+		return img3000X3000;
 	}
 
-	if (+sideXInput.value === 3000 && +sideYInput.value > 3000) {
-		return '2000X4000';
+	if (+sideXInput.value <= 3000 && +sideYInput.value > 3000) {
+		return img2000X4000;
 	}
 
 	if (
@@ -18,7 +29,7 @@ export function choosingImage() {
 		+sideXInput.value < 3400 &&
 		+sideYInput.value > 3000
 	) {
-		return '3300X4000';
+		return img3300X4000;
 	}
 
 	if (
@@ -26,7 +37,7 @@ export function choosingImage() {
 		+sideXInput.value < 3400 &&
 		+sideYInput.value < 3000
 	) {
-		return '3300X2000';
+		return img3300X2000;
 	}
 
 	if (
@@ -34,24 +45,20 @@ export function choosingImage() {
 		+sideXInput.value < 3400 &&
 		+sideYInput.value === 3000
 	) {
-		return '3300X3000';
+		return img3300X3000;
 	}
 
 	if (+sideXInput.value >= 3400 && +sideYInput.value < 3000) {
-		return '4000X2000';
+		return img4000X2000;
 	}
 
 	if (+sideXInput.value >= 3400 && +sideYInput.value === 3000) {
-		return '4000X3000';
-	}
-
-	if (+sideXInput.value < 3000 && +sideYInput.value > 3000) {
-		return '2000X4000';
+		return img4000X3000;
 	}
 
 	if (+sideXInput.value < 3000 && +sideYInput.value === 3000) {
-		return '2000X3000';
+		return img2000X3000;
 	}
 
-	return '2500X2000';
+	return img2500X2000;
 }
