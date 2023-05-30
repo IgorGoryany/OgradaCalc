167eece300c4af42074a4ed128e0aa922f899bda
IgorGoryany <128911252+IgorGoryany@users.noreply.github.com>
2023-05-30 14:36:26 +0300

    Create sizesSupp.js

---
+++ js/support/sizesSupp.js
@@ -0,0 +1,13 @@
+import {sideXInput, sideYInput} from "../variables/variables";
+import img2000X4000 from "../../img/2000X4000.svg";
+
+export function sizesSupp(image) {
+	let sizes = `<span class="top">${sideXInput.value}</span>
+					 <span class="left">${sideYInput.value}</span>`;
+
+	if (image === img2000X4000) {
+		sizes = `<span class="top">${sideYInput.value}</span>
+					<span class="left">${sideXInput.value}</span>`;
+	}
+	return sizes
+}
