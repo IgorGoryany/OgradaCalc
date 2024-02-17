67095c6ea343690a8794395334c773deabe26a87
IgorGoryany <Mr.Q.U.B.E31@gmal.com>
2024-02-18 02:22:52 +0300

    fixed the styles and wrapped the footer in memo

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
+import { memo } from 'react';
+
 import { Container, Text } from '@/shared/ui';
 
 import styles from './Footer.module.scss';
 
-export const Footer = () => (
-  <footer className={styles.footer}>
-    <Container center className={styles.container}>
-      <Text align="right">© {new Date().getFullYear()} ИП "Горяный"</Text>
-    </Container>
-  </footer>
-);
+export const Footer = memo(function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <Container center className={styles.container}>
+        <Text align="right">© {new Date().getFullYear()} ИП "Горяный"</Text>
+      </Container>
+    </footer>
+  );
+});
